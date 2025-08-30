6a2f467c41ea04f092343746c5d9a9a7810feed4
Cien030 <700045954@student.curtin.edu.my>
2025-08-30 17:26:08 +0800

    implement Duplex client

---
+++ MKXLobbyContracts/ILobbyServiceCallback.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using MKXLobbyModels;
+
+namespace MKXLobbyContracts
+{
+    [ServiceContract]
+    public interface ILobbyServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void OnRoomListUpdated(List<LobbyRoom> rooms);
+
+        [OperationContract(IsOneWay = true)]
+        void OnRoomDataUpdated(string roomName, List<ChatMessage> messages, List<string> players, List<SharedFile> files);
+
+        [OperationContract(IsOneWay = true)]
+        void OnPrivateMessageReceived(ChatMessage message);
+
+        [OperationContract(IsOneWay = true)]
+        void OnPlayerJoinedRoom(string roomName, string username);
+
+        [OperationContract(IsOneWay = true)]
+        void OnPlayerLeftRoom(string roomName, string username);
+    }
+}
---
+++ MKXLobbyContracts/ILobbyServiceDuplex.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using MKXLobbyModels;
+
+namespace MKXLobbyContracts
+{
+    [ServiceContract(CallbackContract = typeof(ILobbyServiceCallback))]
+    public interface ILobbyServiceDuplex
+    {
+        //user management
+        [OperationContract]
+        bool LoginPlayer(string username);
+
+        [OperationContract]
+        void LogoutPlayer(string username);
+
+        [OperationContract]
+        List<string> GetOnlinePlayers();
+
+        //room management
+        [OperationContract]
+        bool CreateRoom(string roomName, string createdBy, string username);
+
+        [OperationContract]
+        List<LobbyRoom> GetAvailableRooms();
+
+        [OperationContract]
+        bool JoinRoom(string roomName, string username);
+
+        [OperationContract]
+        void LeaveRoom(string username);
+
+        [OperationContract]
+        List<string> GetPlayersInRoom(string roomName);
+
+        //messaging
+        [OperationContract]
+        void SendMessage(ChatMessage message);
+
+        [OperationContract]
+        List<ChatMessage> GetRoomMessages(string roomName);
+
+        [OperationContract]
+        List<ChatMessage> GetPrivateMessages(string username);
+
+        //file sharing
+        [OperationContract]
+        bool ShareFile(SharedFile file);
+
+        [OperationContract]
+        List<SharedFile> GetSharedFiles(string roomName);
+
+        [OperationContract]
+        SharedFile DownloadFile(string fileName, string roomName);
+
+        //duplex client update
+        [OperationContract]
+        void SubscribeToUpdate(string username);
+
+        [OperationContract]
+        void UnsubscribeFromUpdate(string username);
+    }
+}
